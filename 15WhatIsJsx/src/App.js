16ec364c7663d4c330cfe76e94e632da8b1b6538
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-06-11 05:36:43 +0530

    push

---
+++ 15WhatIsJsx/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <ul>
+        <li>Amy</li>
+        <li>Ben</li>
+        <li>Charles</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
